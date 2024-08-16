e56de165eb9d8a52c45f7d56c3442e6ea0266468
Denis <deniszolotoverh@gmail.com>
2024-08-16 21:23:55 +0300

    feat(mongo): add mongo storage

---
+++ src/db/mongo/mongo.entity.ts
@@ -0,0 +1,18 @@
+import { Document, Types } from 'mongoose';
+import { Prop, SchemaFactory } from '@nestjs/mongoose';
+import { BaseEntity } from '../../shared/storage';
+
+export interface BaseMongoEntity extends BaseEntity {
+  _id: Types.ObjectId;
+  createdAt: Date;
+}
+
+export class MongoEntity extends Document implements BaseMongoEntity {
+  @Prop({ type: Types.ObjectId, default: () => new Types.ObjectId() })
+  _id: Types.ObjectId;
+
+  @Prop({ default: () => new Date(), type: Date })
+  createdAt: Date;
+}
+
+export const MongoSchema = SchemaFactory.createForClass(MongoEntity);
---
+++ src/db/mongo/mongo.storage.ts
@@ -0,0 +1,44 @@
+import { BaseStorage } from '../../shared/storage';
+import { MongoEntity } from './mongo.entity';
+import { Model, Types } from 'mongoose';
+
+/**
+ * Class representing a generic MongoDB storage.
+ * @template T The type of the entity to be stored.
+ */
+
+export class MongoStorage<T extends MongoEntity> extends BaseStorage<T, Types.ObjectId> {
+  protected entityModel: Model<T>;
+
+  async create(data: Partial<T>): Promise<T> {
+    return (await this.entityModel.create(data))
+      .toObject();
+  }
+
+
+  async findById(id: Types.ObjectId): Promise<T | null> {
+    return await this.entityModel
+      .findById(id)
+      .select('-__v')
+      .lean()
+      .exec() as T;
+  }
+
+  async update(id: Types.ObjectId, data: Partial<T>): Promise<T> {
+    return await this.entityModel.findOneAndUpdate(
+      { _id: id },
+      data,
+      { new: true })
+      .lean()
+      .select('-__v')
+      .exec() as T;
+  }
+
+  async delete(id: Types.ObjectId): Promise<void> {
+    await this.entityModel
+      .deleteOne(
+        { _id: id })
+      .exec();
+  }
+
+}
